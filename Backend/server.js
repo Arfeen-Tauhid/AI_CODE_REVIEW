e5786c4b01599e16ed2d64c303c61128df696f1c
Arfeen-Tauhid <arfeentauhid09@gmail.com>
2025-02-24 00:00:40 +0530

    Issue Solve

--- Backend/server.js
+++ Backend/server.js
@@ -4,8 +4,16 @@ const aiRoutes = require ('./src/routes/ai.routes')
 const cors = require('cors')
 const app = express();
 const corsOptions = {
-  origin: process.env.FRONTEND_URL1||FRONTEND_URL2||FRONTEND_URL3,
-  optionsSuccessStatus: 200 
+  origin: (origin, callback) => {
+    const allowedOrigins = [process.env.FRONTEND_URL1||process.env.FRONTEND_URL2||process.env.FRONTEND_URL3];
+    
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
