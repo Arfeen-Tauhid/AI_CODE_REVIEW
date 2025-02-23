7590308d149359dbc309717700845c30591b2cd0
Arfeen-Tauhid <arfeentauhid09@gmail.com>
2025-02-23 23:57:14 +0530

    Issue Solve

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const aiRoutes = require ('./src/routes/ai.routes')
 const cors = require('cors')
 const app = express();
-app.use(cors())
+const corsOptions = {
+  origin: process.env.FRONTEND_URL1||FRONTEND_URL2||FRONTEND_URL3,
+  optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/ai',aiRoutes)
 app.get('/', (req, res) => {
